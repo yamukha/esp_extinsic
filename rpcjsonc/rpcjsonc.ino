0d4425f30eb66e2d551ae777ff350e55b263f5c1
Andriy <yamukha@ukr.net>
2022-04-07 09:34:50 +0300

    created call for Balance transfer

--- rpcjsonc/rpcjsonc.ino
+++ rpcjsonc/rpcjsonc.ino
@@ -24,20 +24,28 @@
 
 // commment to old json parsing (used with remote robonomics), uncomment to new one (with localhost)
 #define RESPONSE_STRING_ARRAY
-#ifdef RESPONSE_STRING_ARRAY
+
+//#define RPC_TO_LOCAL
+#ifdef RPC_TO_LOCAL
+#define GENESIS_HASH     "c0ef85b9b694feb3f7e234b692982c9ae3a166af7b64360da8b7b6cb916e83b6"
+#define CALL_BALANCE_TX  "0700008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4804"
+#define URLRPC "http://192.168.0.102:9933"
+#define TRANSACTION "http://192.168.0.102:9933"
 //#define URLRPC "http://192.168.2.26:9933"
 //#define URLRPC "http://192.168.2.25:9933"
 //#define URLRPC "http://192.168.0.102:9933"
 //#define URLRPC "http://192.168.0.103:9933"
 //#define URLRPC "http://192.168.1.35:9933"
-//#define URLRPC "http://192.168.0.102:9933"
-//#define TRANSACTION "http://192.168.0.102:9933"
-#define URLRPC "http://kusama.rpc.robonomics.network:80/rpc/"
-#define TRANSACTION "http://kusama.rpc.robonomics.network:80/rpc/"
 #else
-#define URLRPC "http://kusama.rpc.robonomics.network:80/rpc/"
-#define TRANSACTION "http://kusama.rpc.robonomics.network:80/rpc/"
+#define GENESIS_HASH     "631ccc82a078481584041656af292834e1ae6daab61d2875b4dd0c14bb9b17bc"
+#define CALL_BALANCE_TX  "1f00008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4804"
+#define URLRPC "http://kusama.rpc.robonomics.network/rpc/"
+#define TRANSACTION "http://kusama.rpc.robonomics.network/rpc/"
 #endif
+
+#define BLOCK_HASH   "0xadb2edbde7e96a00d8c2fe37916bd76d395710d7f794d86c7339066b814f60d9"
+#define SS58DEST     "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty"
+#define SS58KEY      "8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48"
 
 typedef struct uint128_s
 {
@@ -61,13 +69,14 @@ static constexpr uint8_t sigTypeEd25519 = 0x00;
 static constexpr uint32_t multiAddrSpecVersion = 28;
 static constexpr uint32_t multiAddrSpecVersionKsm = 2028;
 static constexpr TWSS58AddressType network = TWSS58AddressTypeKusama;
-
+            
 #define GET_PAYLOAD "{ \"jsonrpc\":\"2.0\", \"id\":1, \"method\":\"get_payload\", \"params\": [\"4GiRoiHkwqYdFpNJWrJrzPcRqaNWJayG1Lq5ZgLqoZwrZHjj\"]}"
 #define SYS_VERSION "{ \"jsonrpc\":\"2.0\", \"id\":1, \"method\":\"system_version\", \"params\": [] }"
 
 bool isGetParameters = true;
 Data edata;
 uint64_t id_conter = 0; 
+uint64_t fee = 0; 
 uint8_t sig[64];
 uint8_t privateKey[32];
 uint8_t publicKey[32];
@@ -103,13 +112,6 @@ inline void encode32LE(uint32_t val, std::vector<uint8_t>& data) {
     data.push_back(static_cast<uint8_t>((val >> 16)));
     data.push_back(static_cast<uint8_t>((val >> 24)));
 }
-
-// ! need to rework !
-/*inline Data encodeCompact(uint32_t value) {
-    auto data = Data{};
-    encode32LE(value, data);
-    return data;
-} */
 
 // only up to uint64_t
 inline Data encodeCompact(uint64_t value) {
@@ -154,6 +156,29 @@ inline void encodeLengthPrefix(Data& data) {
     data.insert(data.begin(), prefix.begin(), prefix.end());
 }
 
+std::vector<uint8_t> hex2bytes (std::string hex) {
+    std::vector<uint8_t> bytes;
+    for (unsigned int i = 0; i < hex.length(); i += 2) {
+      std::string byteString = hex.substr(i, 2);
+      uint8_t byte = (uint8_t) strtol(byteString.c_str(), NULL, 16);
+      bytes.push_back(byte);
+    }
+    return bytes;
+}
+
+std::string swapEndian(String str) {
+    std::string hex = str.c_str();
+    std::string bytes;
+    for (unsigned int i = 0; i < hex.length(); i += 2) {
+      std::string s = hex.substr(i, 2);
+      if ( !(strstr(s.c_str(),"0x")) && !(strstr(s.c_str(),"0X")) ) {
+        std::string byteString = hex.substr(i, 2);
+        bytes.insert(0,byteString);
+      }
+    }
+    return bytes;
+}
+
 void setup() {
 
   Serial.begin(115200);
@@ -169,22 +194,23 @@ void setup() {
   Serial.printf ("Connected to SSID %s own IP address \n", STASSID);
   Serial.println(WiFi.localIP());
 
-  Ed25519::generatePrivateKey(privateKey);
-  Ed25519::derivePublicKey(publicKey, privateKey);
-  //Serial.println(sig);
+  //Ed25519::generatePrivateKey(privateKey);
+  std::vector<uint8_t> vk = hex2bytes("da3cf5b1e9144931a0f0db65664aab662673b099415a7f8121b7245fb0be4143");
+  std::copy(vk.begin(), vk.end(), privateKey);
+  
+  Ed25519::derivePublicKey(publicKey, privateKey);  
+  //std::vector<uint8_t> pk = hex2bytes("f90bc712b5f2864051353177a9d627605d4bf7ec36c7df568cfdcea9f237c185");
+  //std::copy(pk.begin(), pk.end(), publicKey);  
 }
   
 void loop() {
-  uint128_t x = {0,1};
-  String block_hash = "0x631ccc82a078481584041656af292834e1ae6daab61d2875b4dd0c14bb9b17bc";
-  
+    
   if ((WiFi.status() == WL_CONNECTED)) {
 
     WiFiClient client;
     HTTPClient http;
     std::string param0;
     Data data;
-    Data call;
                        
     JSONVar params; 
     String jsonString;
@@ -198,19 +224,7 @@ void loop() {
         get_payload["params"] = params;
         jsonString = JSON.stringify(get_payload);
     } else {
-      // fill params as hex
-      /*
-        char param0 [2 * edata.size() + 2];
-        param0 [0] = '0'; 
-        param0 [1] = 'x'; 
-        for (int i = 0; i < edata.size(); i++) {
-           char ch [2]; 
-           sprintf(ch,"%02x",edata[i]);
-           param0[i*2 + 2] = ch[0];
-           param0[i*2 + 3] = ch[1];
-        }
-        params [0] = param0;
-        */
+      // fill params as hex     
         param0.append("0x");
         char ch [2];   
         for (int i = 0; i < edata.size();i++) {
@@ -218,12 +232,7 @@ void loop() {
                 param0.append(ch);
         }
         params [0] = param0.c_str();
-        //params [0] = "0x3902840016eb796bee0c857db3d646ee7070252707aec0c7d82b2eda856632f6a2306a5801566e85037eee8e228414f49c797ef01e086364783b21a3b9910dcfcf600e1c0fca9207805301fad5da07c50d21e1358c734bcdcc2bdd9b29f969d188c817918b5401f4001f030016eb796bee0c857db3d646ee7070252707aec0c7d82b2eda856632f6a2306a58025a6202";
-        // sample - {"jsonrpc":"2.0","error":{"code":1010,"message":"Invalid Transaction","data":"Transaction has a bad signature"},"id":1}
-        // local  - {"jsonrpc":"2.0","error":{"code":1002,"message":"Verification Error: Runtime error: Execution failed: Execution aborted due to trap: wasm trap: wasm `unreachable` instruction executed\nWASM backtrace:\n\n    0: 0x1c254a - <unknown>!rust_begin_unwind\n    1: 0x2934 - <unknown>!core::panicking::panic_fmt::h6314b5c91abe7349\n    2: 0x4b559 - <unknown>!TaggedTransactionQueue_validate_transaction\n","data":"Runtime error: Execution failed: Execution aborted due to trap: wasm trap: wasm `unreachable` instruction executed\nWASM backtrace:\n\n    0: 0x1c254a - <unknown>!rust_begin_unwind\n    1: 0x2934 - <unknown>!core::panicking::panic_fmt::h6314b5c91abe7349\n    2: 0x4b559 - <unknown>!TaggedTransactionQueue_validate_transaction\n"},"id":1}
-        // remote - {"jsonrpc":"2.0","error":{"code":1002,"message":"Verification Error: Runtime error: Execution failed: Error calling api function: Failed to convert parameter `tx` from node to runtime of validate_transaction","data":"Runtime error: Execution failed: Error calling api function: Failed to convert parameter `tx` from node to runtime of validate_transaction"},"id":1}
-        // then   - {"jsonrpc":"2.0","error":{"code":1012,"message":"Transaction is temporarily banned"},"id":1}
-               
+          
         JSONVar extrinsic;        
         extrinsic["jsonrpc"] = "2.0";
         extrinsic["id"] = (double) id_conter;
@@ -274,21 +283,15 @@ void loop() {
          bool res = false;
          JSONVar val;
          String genesis_hash;
-         uint32_t version = 0;     // transaction version 
-
-#ifdef RESPONSE_STRING_ARRAY         
-         String nonce;
-         String tip;     // uint256_t tip;    // balances::TakeFees   
-         String specVersion; // Runtime spec version 
-         String era;
-         String tx_version;         
-#else
+         String block_hash;
+         uint32_t version = 0;  // transaction version 
          uint64_t nonce;
-         uint64_t tip;     // uint256_t tip;    // balances::TakeFees   
-         uint32_t specVersion; // Runtime spec version 
+         uint64_t tip;          // uint256_t tip;    // balances::TakeFees   
+         uint32_t specVersion;  // Runtime spec version 
          String era;
          uint32_t tx_version;
-#endif                  
+         uint32_t eraI;
+                  
          //"result" or "error"
          for (int i = 0; i < keys.length(); i++) {
              JSONVar value = myObject[keys[i]];
@@ -311,144 +314,146 @@ void loop() {
               //  ["0x631ccc82a078481584041656af292834e1ae6daab61d2875b4dd0c14bb9b17bc",0,16,0,"Immortal",1]
               //  get: nonce, spec_version, tip, era, tx_version
               // ["0x00","0x01000000","0x00","0x0000000000000000","0x0100000000000000"]
-                            
-              String block_hash = "0x631ccc82a078481584041656af292834e1ae6daab61d2875b4dd0c14bb9b17bc";
+  
 #ifdef RESPONSE_STRING_ARRAY                  
-              genesis_hash =  block_hash;
-              nonce =  (const char*) (val[0]);
-              specVersion = (const char*)(val[1]);
-              tip =  (const char*)(val[2]);
+              genesis_hash =  GENESIS_HASH;
+              String nonce_ =  (const char*) (val[0]);
+              String specVersion_ = (const char*)(val[1]);
+              String tip_ =  (const char*)(val[2]);
               era =  (const char*)(val[3]); 
-              tx_version = (const char*) (val[4]);
+              String tx_version_ = (const char*) (val[4]);
+
+              std::string nonceS  = swapEndian (nonce_);
+              nonce =  strtol(nonceS.c_str(), NULL, 16);
+
+              std::string tipS = swapEndian (tip_);
+              tip =  strtol(tipS.c_str(), NULL, 16);
+
+              std::string specVer = swapEndian (specVersion_);
+              specVersion =  strtol(specVer.c_str(), NULL, 16);
+
+              std::string txVer = swapEndian (tx_version_);
+              tx_version = strtol(txVer.c_str(), NULL, 16); 
+  
+              std::string eraS = swapEndian (era);
+              eraI = strtol(eraS.c_str(), NULL, 16); 
+                                      
 #else
+              genesis_hash = (const char*)(val[0]); // or block_hash ? 
               nonce =  long (val[1]);
               specVersion = int(val[2]);
               tip =  long (val[3]);
               era =  (const char*)(val[4]); 
               tx_version = int (val[5]);
+              
+              if (strstr(era.c_str(),"Immortal")) {
+                eraI = 0;
+              }
+              else {
+                std::string eraS = swapEndian (era);
+                eraI = strtol(eraS.c_str(), NULL, 16); 
+              } 
 #endif 
               Serial.println(val);
               Serial.println(genesis_hash);
-              Serial.println(nonce);
-              Serial.println(specVersion);
-              Serial.println(tip);
-              Serial.println(era);
-              Serial.println(tx_version);
-              
-              // encodePayload();              
-              append(data, call);  
-#ifdef RESPONSE_STRING_ARRAY
-              // encodeEraNonceTip()              
-              /*
-              append(data, era, 2);
-              append(data, nonce, 2);
-              append(data, tip, 2);
-              append(data, specVersion, 2);
-              */
-              
-              /*
-              std::vector<uint8_t> vec;       
-              vec.assign(era.begin() + 2, era.end());
-              append(data, vec);
-
-              std::vector<uint8_t> vec1;
-              vec1.assign(nonce.begin() + 2, nonce.end());
-              append(data, vec1);
-             
-              std::vector<uint8_t> vec2;
-              vec2.assign(tip.begin() + 2, tip.end());
-              append(data, vec2);
-
-              std::vector<uint8_t> vec3;
-              vec3.assign(specVersion.begin() + 2, specVersion.end());
-              append(data, vec3);
-              */
-              append(data, encodeCompact(0)); // era
-              append(data,encodeCompact(0));  // nonce
-              append(data, encodeCompact(0)); //tip
-              encode32LE(1, data);           // specversion
-              encode32LE(1, data); // version
-              genesis_hash = "0x631ccc82a078481584041656af292834e1ae6daab61d2875b4dd0c14bb9b17bc";
-              std::vector<uint8_t> genesisHash; 
-              genesisHash.assign(genesis_hash.begin(), genesis_hash.end());
-              append(data, genesisHash);
-              std::vector<uint8_t> blockHash;  
-              blockHash.assign(block_hash.begin(), block_hash.end());
-              append(data,  blockHash);       
+              Serial.printf("nonce: %ld\n",nonce);
+              Serial.printf("specVersion: %ld\n",specVersion);
+              Serial.printf("tip: %ld\n",tip); 
+              Serial.printf("era: %ld\n",eraI);
+              Serial.printf("tx_version: %ld\n",tx_version);
+                            
+              //  ==== encodePayload() ===            
+              //  == build  call ==             
+              // Data call = Data{0x04, 0x00}; // Balance.Transfer //  append(data, getCallIndex(network, balanceTransfer));
+#ifdef RPC_TO_LOCAL
+              Data call = Data{7, 0,0};
 #else
-              /*std::vector<uint8_t> vec;       // encodeEraNonceTip()
-              vec.assign(era.begin(), era.end()); // era
-              append(data, vec);
-              */
-              append(data, encodeCompact(0)); //  era "Immortal"
+              Data call = Data{0x1f, 0,0};
+#endif
+              // append(call, encodeAccountId(address.keyBytes(), encodeRawAccount(network, specVersion))); 
+              std::vector<uint8_t> dst = hex2bytes (SS58KEY); // from SS58DST derived SS58KEY
+              append(call, dst); 
+              append(call, encodeCompact(++fee)); // value
+              append(data, call);               
+              
+#ifdef RESPONSE_STRING_ARRAY
+              // == encodeEraNonceTip() == 
+              append(data, encodeCompact(eraI)); // era; note: it simplified to encode, maybe need to rewrite
+              append(data,encodeCompact(nonce)); // nonce
+              append(data, encodeCompact(tip)); //tip
+              
+              encode32LE(specVersion, data);    // specversion
+              encode32LE(tx_version, data);     // version 
+
+              std::vector<uint8_t> gh = hex2bytes(GENESIS_HASH);
+              append(data, gh);
+              append(data, gh);              
+#else
+              append(data, encodeCompact(eraI)); // era; note: it simplified to encode, maybe need to rewrite
               append(data, encodeCompact(nonce));
               append(data, encodeCompact(tip));
-              encode32LE(specVersion, data);
-              encode32LE(version, data);
               
-              genesis_hash =  (const char*)(val[0]);                         
-              std::vector<uint8_t> genesisHash; 
-              genesisHash.assign(genesis_hash.begin(), genesis_hash.end());
-              append(data, genesisHash);
-              std::vector<uint8_t> blockHash;  
-              blockHash.assign(block_hash.begin(), block_hash.end());
-              append(data,  blockHash);              
+              encode32LE(specVersion, data);  // specversion
+              encode32LE(tx_version, data);      // version
+            
+              std::vector<uint8_t> gh = hex2bytes(GENESIS_HASH);
+              append(data, gh);
+              append(data, gh);     
 #endif
               // sign payload
-              uint8_t payload[data.size()];
+              uint8_t payload[data.size()];             
+      
               std::copy(data.begin(), data.end(), payload);
               Ed25519::sign(sig, privateKey, publicKey, payload, data.size());
+              Serial.printf("\Payload raw: len %d , data:\n", data.size());
+              for (int i = 0; i < data.size(); i++) {
+                Serial.printf("%02x",payload[i]);
+              }
+              Serial.printf("\nSigned payload: len %d , data:\n", 64);
+              for (int i = 0; i < 64; i++) {
+                Serial.printf("%02x",sig[i]);
+              }
               
-              // encodeSignature(publicKey, signature); 
+              // == encodeSignature(publicKey, signature) ; == 
               append(edata, Data{extrinsicFormat | signedBit});  // version header
-
+              append(edata,0);
+             
               //append(edata, encodeAccountId(publicKey.bytes, encodeRawAccount(network, specVersion)));  // signer public key
 #ifdef RESPONSE_STRING_ARRAY
-              bool etype =  encodeRawAccount(network, 1); // specVersion = 1
+              bool etype =  encodeRawAccount(network, 16); // specVersion = 1
 #else 
               bool etype =  encodeRawAccount(network, specVersion);
 #endif    
+              Serial.printf("\nAccountID type %d, data:\n", etype);
+              for (int i = 0; i < 32; i++) {
+                Serial.printf("%02x",publicKey[i]);
+              }
+              Serial.println("");
+    
               std::vector<std::uint8_t> pubKey( reinterpret_cast<std::uint8_t*>(std::begin(publicKey)), reinterpret_cast<std::uint8_t*>(std::end(publicKey)));
-              append(edata, encodeAccountId(pubKey,etype));  // signer public key
+              //append(edata, encodeAccountId(pubKey,etype));  // signer public key
+              append(edata,pubKey);  // signer public key
+              
               append(edata, sigTypeEd25519); // signature type
-              std::vector<byte> signature (sig,sig + 64);        // signanture is data
-              append(edata, signature);      // signature
+              std::vector<byte> signature (sig,sig + 64);    // signanture is signed data
+              append(edata, signature);      // signatured payload
               
               // era / nonce / tip // append(edata, encodeEraNonceTip());
 #ifdef RESPONSE_STRING_ARRAY
-              /*
-              append(edata, era, 2);
-              append(edata, nonce, 2);
-              append(edata, tip, 2);      
-              */
-              /*
-              std::vector<uint8_t> vec7;       // encodeEraNonceTip()
-              vec7.assign(era.begin() + 2, era.end());
-              append(edata, vec7);
-
-              std::vector<uint8_t> vec8;
-              vec8.assign(nonce.begin() + 2, nonce.end());
-              append(edata, vec8);
-              
-              std::vector<uint8_t> vec9;
-              vec9.assign(tip.begin() + 2, tip.end());
-              append(edata, vec9);
-              */
-              append(edata, encodeCompact(0)); // era
-              append(edata,encodeCompact(0));  // nonce
-              append(edata, encodeCompact(0)); //tip
-              
+              append(edata, encodeCompact(eraI)); // era; note: it simplified to encode, maybe need to rewrite
+              append(edata, encodeCompact(nonce)); 
+              append(edata, encodeCompact(tip));                            
 #else
-              append(edata, encodeCompact(0)); //  era "Immortal"
+              append(edata, encodeCompact(eraI)); // era; note: it simplified to encode, maybe need to rewrite
               append(edata, encodeCompact(nonce));
               append(edata,encodeCompact(tip));
-#endif              
-              append(edata, call); // call
+#endif        
+              append(edata, call);
               encodeLengthPrefix(edata); // append length
                             
               char pl[edata.size()];
               std::copy(edata.begin(), edata.end(), pl);
-              Serial.println(edata.size());
+              Serial.printf("size %d\n", edata.size());
               for (int i = 0; i < edata.size();i++) {
                 Serial.printf("%02x",pl[i]);
               }              
