da2e3117d9d42a9e9a7b030b9288dfd7d2c5eb5e
Andriy <yamukha@ukr.net>
2022-05-01 12:44:04 +0300

    convert to one thread app

--- rpcjsonc/rpcjsonc.ino
+++ rpcjsonc/rpcjsonc.ino
@@ -1,59 +1,18 @@
 #include <vector>
 #include <string>
-#include <Scheduler.h>
 
 #include <Arduino.h>
-
-#include <RpcRobonomics.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
+#include <RpcRobonomics.h>
+
 #ifndef STASSID
-//#define STASSID "AirTies_Air4240" 
 #define STASSID "FLY-TL-WR741ND"
 #define STAPSK  "xxxxxxxxxx"
 #endif
 
-uint64_t id_counter = 50; 
-
-class RpcTask : public Task {
-  protected:
-    void setup() {
-       Serial.println("RPC task init");  
-    }
-
-    void loop() {
-      if ((WiFi.status() == WL_CONNECTED)) { 
-        WiFiClient client;
-        Serial.println("RPC task run");
-        RobonomicsRpc rpcProvider(client, URLRPC, PRIVKEY, id_counter);
-        RpcResult r = rpcProvider.DatalogRecord(std::to_string(id_counter)); // id_counter as payload just for example
-        id_counter = id_counter + 2;
-        Serial.printf("[RPC] %ld %s\n", r.code, r.body.c_str());  
-        delay(1000);
-      }
-    }
-
-  private:
-    uint8_t state;
-} rpcTask;
-
-class MainTask : public Task {
-  void setup() {
-     Serial.println("init MainTask");
-  }
-  
-  void loop() {
-   if ((WiFi.status() == WL_CONNECTED)) {
-     Serial.printf("[MAIN] %ld\n", id_counter++);
-     delay(2000);
-   }// WiFI
-  } 
-
-  private:
-    uint8_t state;
-} mainTask;
+uint64_t id_counter = 0; 
 
 void setup() {
 
@@ -70,9 +29,16 @@ void setup() {
   Serial.printf ("Connected to SSID %s own IP address \n", STASSID);
   Serial.println(WiFi.localIP());
 
-  Scheduler.start(&mainTask);
-  Scheduler.start(&rpcTask);
-  Scheduler.begin();
 }
 
-void loop () {}
+void loop () {
+    if ((WiFi.status() == WL_CONNECTED)) { 
+        WiFiClient client;
+        Serial.println("RPC task run");
+        RobonomicsRpc rpcProvider(client, URLRPC, PRIVKEY, id_counter);
+        RpcResult r = rpcProvider.DatalogRecord(std::to_string(id_counter)); // id_counter as payload just for example
+        id_counter = id_counter + 2;
+        Serial.printf("[RPC] %ld %s\n", r.code, r.body.c_str());  
+        delay(1000);
+    }
+}
