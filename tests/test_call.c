cd1d667f8e3e1724488e6bc4917d003b7d4a2fe2
John Doe <yamukha@ukr.net>
2022-09-04 09:16:18 +0300

    refactor to compile test with callDatalogRecord()

--- tests/test_call.c
+++ tests/test_call.c
@@ -1,7 +1,10 @@
 #include  "../libraries/RpcRobonomics/Data.h"
+#include  "../libraries/RpcRobonomics/Call.h"
 
 void test () {
+  auto record = "42";
   Data head_dr = Data{0x33,0};
+  Data call = callDatalogRecord(head_dr, record);
 }
 
 int main () {
